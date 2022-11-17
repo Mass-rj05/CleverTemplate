4082cc815615b9ca0a0eca488f3f0dc534003a92
unknown <krzysztof.mirek.mass@gmail.com>
2022-11-17 20:28:43 +0100

    working on the header

--- js/color_on_scroll.js
+++ js/color_on_scroll.js
@@ -3,11 +3,11 @@ $(window).scroll(function(){
 
     if (window.screen.width > 768)
     {
-        $('nav').toggleClass('scrolled', $(this).scrollTop() > 750)
-    $('nav a').toggleClass('scrolled', $(this).scrollTop() > 750)
-    $('.nav_links').toggleClass('scrolled', $(this).scrollTop() > 750)
-    $('.burger div').toggleClass('scrolled', $(this).scrollTop() > 750)
-    $('.upbutton a').toggleClass('show', $(this).scrollTop() > 750)
+        $('nav').toggleClass('scrolled', $(this).scrollTop() > 50)
+    $('nav a').toggleClass('scrolled', $(this).scrollTop() > 50)
+    $('.nav_links').toggleClass('scrolled', $(this).scrollTop() > 50)
+    $('.burger div').toggleClass('scrolled', $(this).scrollTop() > 50)
+    $('.upbutton a').toggleClass('show', $(this).scrollTop() > 50)
     }
     else
     {
