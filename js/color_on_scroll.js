791284d17b8ce53f223d79add2038f0e8835d6d6
unknown <krzysztof.mirek.mass@gmail.com>
2022-11-18 00:24:23 +0100

    scroll feature

--- js/color_on_scroll.js
+++ js/color_on_scroll.js
@@ -1,23 +1,27 @@
-const onScroll = () => {
-$(window).scroll(function(){
-
-    if (window.screen.width > 768)
-    {
-        $('nav').toggleClass('scrolled', $(this).scrollTop() > 50)
-    $('nav a').toggleClass('scrolled', $(this).scrollTop() > 50)
-    $('.nav_links').toggleClass('scrolled', $(this).scrollTop() > 50)
-    $('.burger div').toggleClass('scrolled', $(this).scrollTop() > 50)
-    $('.upbutton a').toggleClass('show', $(this).scrollTop() > 50)
-    }
-    else
-    {
-        $('nav').toggleClass('scrolled', $(this).scrollTop() > 80)
-        $('nav a').toggleClass('scrolled', $(this).scrollTop() > 80)
-        $('.nav_links').toggleClass('scrolled', $(this).scrollTop() > 80)
-        $('.burger div').toggleClass('scrolled', $(this).scrollTop() > 80)
-        $('.upbutton a').toggleClass('show', $(this).scrollTop() > 80)
-    }
-
+$(document).ready(function () {
+  let zero = 0;
+  $(window).on("scroll", function () {
+    console.log("here")
+    $("nav").toggleClass("hide", $(window).scrollTop() > zero);
+    zero = $(window).scrollTop();
+  });
 });
-}
+
+const onScroll = () => {
+  $(window).scroll(function () {
+    if (window.screen.width > 768) {
+      $("nav").toggleClass("scrolled", $(this).scrollTop() > 50);
+      $("nav a").toggleClass("scrolled", $(this).scrollTop() > 50);
+      $(".nav_links").toggleClass("scrolled", $(this).scrollTop() > 50);
+      $(".burger div").toggleClass("scrolled", $(this).scrollTop() > 50);
+      $(".upbutton a").toggleClass("show", $(this).scrollTop() > 50);
+    } else {
+      $("nav").toggleClass("scrolled", $(this).scrollTop() > 80);
+      $("nav a").toggleClass("scrolled", $(this).scrollTop() > 80);
+      $(".nav_links").toggleClass("scrolled", $(this).scrollTop() > 80);
+      $(".burger div").toggleClass("scrolled", $(this).scrollTop() > 80);
+      $(".upbutton a").toggleClass("show", $(this).scrollTop() > 80);
+    }
+  });
+};
 onScroll();
