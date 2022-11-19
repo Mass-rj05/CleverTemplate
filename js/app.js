0de753b7cbe645eaa6f3b0a2f9f344e7cd07a30c
unknown <krzysztof.mirek.mass@gmail.com>
2022-11-19 10:35:49 +0100

    fixing cards

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav_links');
     const navLinks = document.querySelectorAll('.nav_links li');
-    console.log(navLinks);
    
   
     burger.addEventListener('click', () => {
