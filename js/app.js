91face4e3c08c44c090c82f141dedd90937ca77f
unknown <krzysztof.mirek.mass@gmail.com>
2022-11-20 20:27:33 +0100

    quickfix

--- js/app.js
+++ js/app.js
@@ -20,41 +20,40 @@ const navSlide = () => {
 
 navSlide();
 
-
 const tabs = document.querySelectorAll("[data-tab-target]");
 const tabContents = document.querySelectorAll("[data-tab-content]");
-  tabs.forEach((tab) => {
+tabs.forEach((tab) => {
   tab.addEventListener("click", () => {
-      const target = document.querySelector(tab.dataset.tabTarget);
-      const targetImg = document.querySelector(tab.dataset.tabTarget + "-img");
-      tabContents.forEach((tabContent) => {
+    const target = document.querySelector(tab.dataset.tabTarget);
+    const targetImg = document.querySelector(tab.dataset.tabTarget + "-img");
+    tabContents.forEach((tabContent) => {
       tabContent.classList.remove("active-tab");
-      });
+    });
 
-      tabs.forEach((tab) => {
+    tabs.forEach((tab) => {
       tab.classList.remove("active-tab");
-      });
-      tab.classList.add("active-tab");
-      target.classList.add("active-tab");
-      targetImg.classList.add("active-tab");
+    });
+    tab.classList.add("active-tab");
+    target.classList.add("active-tab");
+    targetImg.classList.add("active-tab");
 
-      const mobileLabel = document.getElementById("mobile-label");
-      if (window.getComputedStyle(mobileLabel).display !== "none") {
+    const mobileLabel = document.getElementById("mobile-label");
+    if (window.getComputedStyle(mobileLabel).display !== "none") {
       mobileLabel.textContent = tab.title;
       showMobileMenu();
-      }
+    }
   });
-  });
+});
 
-
-  
 const showMobileMenu = (listNo) => {
-    if (!listNo) listNo = "";
-    const chevron = document.getElementById("btn-chevron" + listNo);
-    chevron.classList.toggle("open");
-  
-    const mobileList = document.getElementById("methods-list" + listNo);
-    mobileList.classList.toggle("mobile-open");
-  };
-  
-  
+  if (!listNo) listNo = "";
+  const chevron = document.getElementById("btn-chevron" + listNo);
+  chevron.classList.toggle("open");
+
+  const mobileList = document.getElementById("methods-list" + listNo);
+  mobileList.classList.toggle("mobile-open");
+};
+
+const scrollToTheTop = () => {
+  document.body.scrollTop = document.documentElement.scrollTop = 0;
+};
