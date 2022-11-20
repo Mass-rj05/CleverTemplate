3679853bda3d8f86913bfaf5a9a5d4e5c49dbe8e
unknown <krzysztof.mirek.mass@gmail.com>
2022-11-20 19:40:58 +0100

    fixing scrollTop

--- js/color_on_scroll.js
+++ js/color_on_scroll.js
@@ -14,13 +14,13 @@ const onScroll = () => {
       $("nav a").toggleClass("scrolled", $(this).scrollTop() > 50);
       $(".nav_links").toggleClass("scrolled", $(this).scrollTop() > 50);
       $(".burger div").toggleClass("scrolled", $(this).scrollTop() > 50);
-      $(".upbutton a").toggleClass("show", $(this).scrollTop() > 50);
+      $(".upbutton").toggleClass("show", $(this).scrollTop() > 50);
     } else {
       $("nav").toggleClass("scrolled", $(this).scrollTop() > 80);
       $("nav a").toggleClass("scrolled", $(this).scrollTop() > 80);
       $(".nav_links").toggleClass("scrolled", $(this).scrollTop() > 80);
       $(".burger div").toggleClass("scrolled", $(this).scrollTop() > 80);
-      $(".upbutton a").toggleClass("show", $(this).scrollTop() > 80);
+      $(".upbutton").toggleClass("show", $(this).scrollTop() > 80);
     }
   });
 };
